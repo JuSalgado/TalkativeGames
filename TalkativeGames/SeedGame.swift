c3f029a6e8406f2dcb5fa7475c6c0d84170defc6
Juliana Salgado <juju-salgado@hotmail.com>
2015-04-15 15:36:56 -0300

    game structure

---
+++ TalkativeGames/SeedGame.swift
@@ -0,0 +1,36 @@
+//
+//  SeedGame.swift
+//  TalkativeGames
+//
+//  Created by Juliana Salgado on 15/04/15.
+//  Copyright (c) 2015 Juliana Salgado. All rights reserved.
+//
+
+import UIKit
+
+class SeedGame: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        println("SEED ")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
