4a1a4f91692fde3e989d6ec16f1cc683f6851c75
eliasayache <elias_ayache@hotmail.com>
2015-04-16 07:19:32 -0300

    image collision

--- TalkativeGames/SeedGame.swift
+++ TalkativeGames/SeedGame.swift
@@ -10,10 +10,53 @@ import UIKit
 
 class SeedGame: UIViewController {
     
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var firstBlock: UIImageView!
+    
+    @IBOutlet weak var text: UILabel!
+    var location = CGPoint(x: 0,y: 0)
+    /*
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        var touch : UITouch! = touches.first as? UITouch
+        
+        location = touch.locationInView(self.view!)
+        
+        firstImage.center = location
+    }
+    */
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        var touch : UITouch! = touches.first as? UITouch
+        
+        location = touch.locationInView(self.view!)
+        
+        firstImage.center = location
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+    
+        
+        if(CGRectIntersectsRect(firstImage.frame, firstBlock.frame))
+        {
+            text.text="a"
+            //var touch : UITouch! = touches.first as? UITouch
+            
+            //location = touch.locationInView(self.view)
+            
+            firstImage.frame = firstBlock.frame
+        }
+        else
+        {
+            text.text="c"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        firstImage.center = CGPointMake(160, 330)
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,6 +64,18 @@ class SeedGame: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func collision(){
+        text.text="b"
+        
+        if(CGRectIntersectsRect(firstImage.frame, firstBlock.frame)==true)
+        {
+            text.text="a"
+        }
+        else
+        {
+            text.text="c"
+        }
+    }
     
     /*
     // MARK: - Navigation
